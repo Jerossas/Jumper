11fb29f079e3ac86f1322360f46494eca6ca0656
Joan Ferley Mosquera Lozano <mosqueralozanojhoanferley@gmail.com>
2026-07-16 12:38:07 -0500

    feat: add game loop using Thread and Runnable whitin a JPanel
    - Added GamePanel.java with the animation Thread embedded
    - Added Main.java which is the JFrame wrapping the GamePanel
    - Modified gradle wrapper to allow user execution

---
+++ src/main/java/com/dunno/GamePanel.java
@@ -0,0 +1,95 @@
+package com.dunno;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements Runnable {
+
+	private static final int NUMBER_DELAYS_PER_YIELD = 16;
+	private static final int MAX_FRAME_SKIPS = 5;
+	private Thread animator;
+	private long period = 16 * 1_000_000L; // Represents the time for each frame. In this case 60 FPS
+
+
+	private volatile boolean running;
+
+	public GamePanel() {
+		
+	}
+
+	@Override
+	public void addNotify() {
+		
+		super.addNotify();
+		startGame();
+	}
+
+	private void startGame() {
+		
+		if(animator == null || !running) {
+			animator = new Thread(this);
+			animator.start();
+		}
+	}
+
+	@Override
+	public void run(){
+		
+		long initialTime, finalTime, elapsedTime, sleepTime;
+		long overSleepTime = 0L;
+		int numberDelays = 0;
+		long excess = 0L;
+
+		initialTime = System.nanoTime();
+
+		running = true;
+
+		while(running) {
+			
+			// TODO: Update game state
+			// TODO: Render game objects
+			// TODO: Paint objects
+
+			System.out.println("Game is running!");
+			
+			finalTime = System.nanoTime();
+			elapsedTime = finalTime - initialTime;
+			sleepTime = (period - elapsedTime) - overSleepTime;
+
+			if(sleepTime > 0) {
+
+				try {
+					
+					Thread.sleep(sleepTime/1_000_000L);
+				} catch(InterruptedException e) {
+					
+					throw new RuntimeException(e);
+				}
+
+				overSleepTime = (System.nanoTime() - finalTime) - sleepTime;
+			} else {
+
+				excess -= sleepTime;
+				overSleepTime = 0L;
+
+				if(++numberDelays >= NUMBER_DELAYS_PER_YIELD) {
+
+					Thread.yield();
+					numberDelays = 0;
+				}
+			}
+
+			initialTime = System.nanoTime();
+
+			int skips = 0;
+
+			while((excess > period) && (skips < MAX_FRAME_SKIPS)) {
+
+				excess -= period;
+				// TODO: update game state 
+				skips++;
+			}
+		}
+
+		System.exit(0);
+	}
+}
--- src/main/java/com/dunno/Main.java
+++ src/main/java/com/dunno/Main.java
@@ -1,9 +1,23 @@
 package com.dunno;
 
-public class Main {
+import javax.swing.SwingUtilities;
+import javax.swing.JFrame;
+
+public class Main extends JFrame {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello from Jumper!");
+        SwingUtilities.invokeLater(() -> {
+
+		Main main = new Main();
+		main.add(new GamePanel());
+		main.pack();
+
+		main.setTitle("Jumper");
+		main.setLocationRelativeTo(null);
+		main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		main.setVisible(true);
+	});
     }
 }
